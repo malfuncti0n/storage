bbbe8a5f4ac427d3a9d947fb3fc7aed4c29b6128
malfuncti0n <savvaniss@yahoo.gr>
2017-03-21 13:52:34 +0200

    test functions

--- app/Controllers/ArticleController.php
+++ app/Controllers/ArticleController.php
@@ -12,4 +12,12 @@ class ArticleController extends Controller
     public function post(){
         die('post');
     }
+
+    public function put($id){
+        die($id);
+    }
+
+    public function delete($id){
+        die($id);
+    }
 }
