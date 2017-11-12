69a0b824c68620a8abcb2924fd8c05a61fe5d3dd
savvaniss <>
2017-11-12 19:11:21 +0200

    update for google login

--- app/routes.php
+++ app/routes.php
@@ -3,7 +3,6 @@
 //one route will dynamicly calculate the correct method based on method
 $app->any('/api/user[/{username}[/{password}]]', 'UserController:getCall');
 
-$app->post('/api/facebook', 'FacebookUserController:getCall');
 
 
 
